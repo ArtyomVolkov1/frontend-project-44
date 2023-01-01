ef843b97a262edc4b91fa93c7c12dd83d40d92b1
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-02 00:57:57 +0300

    Fixed linter errors

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,11 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 // const readlineSync = require("readline-sync");
 
 const differentUserName = () => {
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('May I have your name? ');
+  let greeting;
   if (userName) {
-    return "Hello, " + userName + "!";
-  }
+    greeting = `Hello, ${userName}!`;
+  } return greeting;
 };
 export default differentUserName;
