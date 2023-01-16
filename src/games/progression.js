6ab7298c1f826f2f309ff024843b9c1e2bf3cc8b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-16 22:20:33 +0300

    Added new game

---
+++ src/games/progression.js
@@ -0,0 +1,35 @@
+import getPlayed from '../index.js'
+
+export const randomNumber = (min, max) => {
+    return min + Math.round(Math.random() * (max - min));
+  };
+
+  const ruleGame = 'What number is missing in the progression?'
+
+  const progression = () => {
+    const lengthProgression = randomNumber(5, 10);
+    let numProgression =  randomNumber(1, 19);
+    const stepPosition = randomNumber(2, 5);
+    const result = [];
+    for (let i = 0; i <= lengthProgression; i += 1) {
+        result.push(numProgression);
+        numProgression += stepPosition;
+    } return result;
+  };  
+  
+  const generateRound = () => {
+    let progressions = progression();
+    let randomNum = randomNumber(1, 9);
+    const correctAnswer = progressions[randomNum];
+    progressions[randomNum] = '..';
+    const question = progressions.join(' ');
+    return [question, String(correctAnswer)];
+  };
+
+  const gameProgression = () => {
+    getPlayed(ruleGame, generateRound);
+  };
+
+  export default gameProgression;
+
+
