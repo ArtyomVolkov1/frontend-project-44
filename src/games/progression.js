94445d22bb076a6f4e1a40c87fea25c340ee1645
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-17 14:00:09 +0300

    Update

--- src/games/progression.js
+++ src/games/progression.js
@@ -7,11 +7,11 @@ export const randomNumber = (min, max) => {
   const ruleGame = 'What number is missing in the progression?'
 
   const progression = () => {
-    const lengthProgression = randomNumber(5, 10);
+    const lengthProgression = 10;
     let numProgression =  randomNumber(1, 19);
     const stepPosition = randomNumber(2, 5);
     const result = [];
-    for (let i = 0; i <= lengthProgression; i += 1) {
+    for (let i = 0; i < lengthProgression; i += 1) {
         result.push(numProgression);
         numProgression += stepPosition;
     } return result;
