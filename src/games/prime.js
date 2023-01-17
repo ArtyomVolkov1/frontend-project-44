d86fd7add40cfd5d446d55319dfe148a164b414b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-17 14:52:11 +0300

    Fix error eslint

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,28 +1,26 @@
+import getPlayed from '../index.js';
 
-import getPlayed from '../index.js'
+const randomNumber = (min, max) => min + Math.round(Math.random() * (max - min));
 
-const randomNumber = (min, max) => {
-    return min + Math.round(Math.random() * (max - min));
-  };
-  
-  const ruleGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const ruleGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  randomNumber(1, 10);
-  const checkPrime = (number) => {
-    for (let i = 2; i < Math.sqrt(number); i += 1) {
-        if (number % i === 0) {
-            return false;
-        }
-    } return true
+const checkPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
     }
-  const generateRound = () => {
-    const question = randomNumber(2, 100);;
-    const correctAnswer = (checkPrime(question)) ? 'yes' : 'no';
-    return [question, correctAnswer];
-  };
-  
-  const gameCheckPrime = () => {
-    getPlayed(ruleGame, generateRound);
-  };
-  
-  export default gameCheckPrime
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = randomNumber(2, 100);
+  const correctAnswer = checkPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const gameCheckPrime = () => {
+  getPlayed(ruleGame, generateRound);
+};
+
+export default gameCheckPrime;
