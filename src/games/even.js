3ddea3be698936ebddfcc437948585982cb52a80
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-17 14:51:27 +0300

    Fix error eslint

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,14 @@
+import getPlayed from '../index.js';
 
-import getPlayed from '../index.js'
-
-const randomNumber = (min, max) => {
-  return min + Math.round(Math.random() * (max - min));
-};
+const randomNumber = (min, max) => min + Math.round(Math.random() * (max - min));
 
 const ruleGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const checkParity = (parity) => {
- return parity % 2 === 0;
-};
+const checkParity = (parity) => parity % 2 === 0;
+
 const generateRound = () => {
   const question = randomNumber(1, 10);
-  const correctAnswer = (checkParity(question)) ? 'yes' : 'no';
+  const correctAnswer = checkParity(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
