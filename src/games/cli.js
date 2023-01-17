ff1d2de9a23e36dc110b2273a6c6660de3b17b54
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-17 17:26:32 +0300

    Fix errors eslint

--- src/games/cli.js
+++ src/games/cli.js
@@ -4,8 +4,7 @@ import readlineSync from 'readline-sync';
 
 const greetingUser = () => {
   const userName = readlineSync.question('May I have your name? ');
-  const greeting = 'Hello, ' + userName + '!';
+  const greeting = `Hello, ${userName}!`;
   console.log(greeting);
-  return;
 };
 export default greetingUser;
