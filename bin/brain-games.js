ce17a9034b310a501488381a58d173da079c9d57
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-21 10:03:36 +0300

    Update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
-import greetingUser from '../src/games/cli.js';
-
-console.log('Welcome to the Brain Games!');
+import greetingUser from '../src/cli.js';
 
 /* Функция вызова приветствия */
 
