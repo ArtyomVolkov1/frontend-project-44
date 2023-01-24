9e9baabfa5458e9cfe5f14a2bd4bd6db1f60d7b4
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-24 22:07:07 +0300

    Update

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,5 @@
 import readlineSync from 'readline-sync';
 
-/* Приветствие пользователя */
-
 const greetingUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
