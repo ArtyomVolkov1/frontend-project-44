108d88688a355d920f13ff85783a1a2ac3e6031f
Artyom Volkov <artemvolk2099@yandex.ru>
2023-01-24 22:07:20 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 
-/* Общая логика для всех игр */
+const roundsCount = 3;
+
 const runEngine = (ruleGame, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(ruleGame);
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const [question, correctAnswer] = generateRound();
     console.log('Question:', question);
     const userAnswer = readlineSync.question('Your answer: ');
